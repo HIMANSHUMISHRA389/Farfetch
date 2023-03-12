eec2ff10e9def9f5d48138c59363b51cc788a46e
manish9427 <manish119427@gmail.com>
2023-03-13 00:52:57 +0530

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
-
+import Footer from "./Components/Footer/Footer";
+import Navbar from "./Components/Navbar/Navbar";
+import AllRoutes from "./Routes/AllRoutes";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <AllRoutes />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
